2eb4b28959aaf8b26735c781e1483095d6ecefc1
Alonso271 <72246922+Alonso271@users.noreply.github.com>
2020-11-09 14:46:39 +0100

    Create funciones.php

---
+++ funciones.php
@@ -0,0 +1,10 @@
+<?php
+function usuarioOk($usuario, $contra) :bool {
+$resu= false;
+    $long = strlen($usuario);
+    if ($long >= 8 && $contra == strrev($usuario)) {
+        $resu = true;
+    }
+return $resu;
+    
+}
